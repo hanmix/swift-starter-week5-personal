05da13a084b1c427a99062e9cbe40ab6d157a1a1
hansm <hanmix2003@naver.com>
2022-06-08 17:54:14 +0900

    Feat: BadPersonality 프로토콜 정의
    - frequencyOfCursing 프로퍼티: Level 타입을 가지는 프로퍼티 정의

---
+++ CodeStarter-Week5/BadPersonality.swift
@@ -0,0 +1,12 @@
+//
+//  BadPersonality.swift
+//  CodeStarter-Week5
+//
+//  Created by mingmac on 2022/06/01.
+//
+
+import Foundation
+
+protocol BadPersonality {
+    var frequencyOfCursing: Level { get }
+}
