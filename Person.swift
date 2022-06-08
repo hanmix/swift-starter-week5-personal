e10955d9f780721159a6627cc27057aedb5f46cb
keyster <82441945+hanmix@users.noreply.github.com>
2022-06-08 17:16:59 +0900

    Feat: Person 클래스 추가
    name 프로퍼티: 인스턴스의 이름을 정의하는 프로퍼티
    height 프로퍼티: 인스턴스의 키를 정의하는 프로퍼티

---
+++ Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  CodeStarter-Week5
+//
+//  Created by mingmac on 2022/06/01.
+//
+
+import Foundation
+
+class Person {
+    let name: String
+    let height: Int
+    
+    init(name: String, height: Int) {
+        self.name = name
+        self.height = height
+    }
+}
