221e0ed858c8631c4907187441cc43921bab7f39
hansm <hanmix2003@naver.com>
2022-06-09 21:25:43 +0900

    Feat: Access Control을 위한 구조체 타입 정의
    - hackPassedApplicantsList 메소드: 합격자명단에 mySon 인스턴스를 추가하기 위한 메소드

---
+++ CodeStarter-Week5/Hacker.swift
@@ -0,0 +1,14 @@
+//
+//  Hacker.swift
+//  CodeStarter-Week5
+//
+//  Created by mingmac on 2022/06/09.
+//
+
+import Foundation
+
+struct Hacker {
+    func hackPassedApplicantsList() {
+        auditionManager.passedApplicatsList.append(mySon)
+    }
+}
